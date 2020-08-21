ae52ab04b9cf4402f5cf0b722c57e8b429deb9e3
Julian David Ramirez Lopera <jdramirezl@eafit.edu.co>
2020-08-21 12:16:56 -0500

    k p2 ya lo subi xd

--- games/settings.py
+++ games/settings.py
@@ -11,10 +11,10 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
@@ -27,8 +27,24 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES":
+        ("rest_framework_simplejwt.authentication.JWTAuthentication",)
+}
+
+SIMPLE_JWT = {
+
+    'ACCESS_TOKEN_LIFETIME':
+        timedelta(hours=10),
+
+    'REFRESH_TOKEN_LIFETIME':
+        timedelta(days=1),
+
+    'ROTATE_REFRESH_TOKENS':
+        True,
+
+}
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -72,7 +88,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'games.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
@@ -82,7 +97,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
@@ -102,7 +116,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
@@ -115,7 +128,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
