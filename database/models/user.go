e060ec601ad8edaca719b5aaa7dde5d7e7957ef0
drew <andresalazar138@gmail.com>
2020-09-19 19:20:38 -0500

    added constraint to user model to have unique email addresses as identifiers

--- database/models/user.go
+++ database/models/user.go
@@ -2,12 +2,11 @@ package models
 
 import "gorm.io/gorm"
 
+// User model
 type User struct {
 	gorm.Model
-	Email    string `json:"email"`
+	Email    string `json:"email" gorm:"unique"`
 	Name     string `json:"name"`
 	Surname  string `json:"surname"`
 	Password string `json:"password"`
 }
-
-// https://stackoverflow.com/questions/23589564/function-for-converting-a-struct-to-map-in-golang
