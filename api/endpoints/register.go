2f746324b06455e1011e44dbd8cbe4a82071515b
drew <andresalazar138@gmail.com>
2020-09-27 17:07:54 -0500

    added jwt keywords in json response for the register handler function

--- api/endpoints/register.go
+++ api/endpoints/register.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/drew138/games/api/authentication"
+	"github.com/drew138/games/api/authorization"
 	"github.com/drew138/games/database"
 	"github.com/drew138/games/database/models"
 	"github.com/gofiber/fiber/v2"
@@ -15,26 +16,33 @@ func CreateUser(c *fiber.Ctx) error {
 	if !HasJSONBody(c) {
 		return fmt.Errorf("Body does not contain JSON format")
 	}
-	user := new(models.User)
-	if UnmarshalJSON(c, &user) {
+	var User models.User
+	if UnmarshalJSON(c, &User) {
 		return fmt.Errorf("Invalid user properties")
 	}
-	validationError := authentication.ValidatePassword(user.Password)
+	validationError := authentication.ValidatePassword(User.Password)
 	if validationError != nil {
 		c.Status(400).Send([]byte(validationError.Error()))
 		return validationError
 	}
-	user.Password = authentication.HashGenerator([]byte(user.Password))
-	dbError := database.DBConn.Create(&user).Error
+	User.Password = authentication.HashGenerator([]byte(User.Password))
+	dbError := database.DBConn.Create(&User).Error
 	if dbError != nil {
 		c.Status(500).Send([]byte(dbError.Error()))
 		return dbError
 	}
+	tokenPair, err := authorization.GenerateJWT(&User)
+	if err != nil {
+		c.Status(500).Send([]byte(err.Error()))
+		return err
+	}
 	userMap := map[string]interface{}{
-		"email":   user.Email,
-		"name":    user.Name,
-		"surname": user.Surname,
-		"isAdmin": user.IsAdmin, //TODO remove this field
+		"email":        User.Email,
+		"name":         User.Name,
+		"surname":      User.Surname,
+		"isAdmin":      User.IsAdmin, //TODO remove this field
+		"accessToken":  tokenPair["accessToken"],
+		"refreshToken": tokenPair["refreshToken"],
 	}
 	c.Status(201)
 	c.JSON(userMap) // convert to json and send response
