5ab387f6c71f08cd0797d156cf3f22f5b0337dba
drew <andresalazar138@gmail.com>
2020-09-27 13:26:12 -0500

    finished ParseJWT function which returns a Token struct

--- api/authorization/parseToken.go
+++ api/authorization/parseToken.go
@@ -1,8 +1,20 @@
 package authorization
 
-import jwt "github.com/dgrijalva/jwt-go"
+import (
+	"fmt"
+	"os"
 
-// ParseJWT - retrieve data from JWT
-func ParseJWT(token string) (string, error) {
-	jwt.Parse(token)
+	jwt "github.com/dgrijalva/jwt-go"
+)
+
+// ParseJWT - parse jwt and return data
+func ParseJWT(token string) (*jwt.Token, error) {
+	tok, err := jwt.Parse(token, func(token *jwt.Token) (interface{}, error) {
+		_, ok := token.Method.(*jwt.SigningMethodHMAC) // handler function as described in documentation
+		if !ok {
+			return nil, fmt.Errorf("Unauthorized token")
+		}
+		return os.Getenv("JWT_SECRET_KEY"), nil
+	})
+	return tok, err
 }
